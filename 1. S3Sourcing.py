051a4b846b3d438bb146ac410627acb4e06c5f23
sam-burns-Git <samburns99h@gmail.com>
2024-10-31 15:28:34 +0000

    Update 1. S3Sourcing.py
    Removing Access Keys from public repo

--- 1. S3Sourcing.py
+++ 1. S3Sourcing.py
@@ -3,8 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 
 # AWS S3 credentials
-s3_client = boto3.client('s3', aws_access_key_id='AKIAUW4RBBFE5L3N4TNZ',
-                         aws_secret_access_key='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',
+s3_client = boto3.client('s3', aws_access_key_id='***',
+                         aws_secret_access_key='******',
                          region_name='eu-north-1') #client to access my S3 bucket
 BUCKET_NAME = 'rearc-quest-sam' #
 BLS_URL = 'https://download.bls.gov/pub/time.series/pr/' #Base URL
